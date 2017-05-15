c068ef877deec9f46512271ee68a1b834f90c404
Valentin Brosseau <c4software@gmail.com>
2017-05-15 21:23:47 +0200

    Update config.py to add the xmlns for image sitemap

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 xml_header = """<?xml version="1.0" encoding="UTF-8"?>
 <urlset
       xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+      xmlns:image="http://www.google.com/schemas/sitemap-image/1.1"
       xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
       xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
             http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
